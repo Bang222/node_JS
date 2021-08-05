e825f954e3b7248a3374ddcab30ff275ba04f6bf
Bang222 <bangdanh105@gmail.com>
2021-08-05 23:27:36 +0700

    update

---
+++ node_sever.js
@@ -0,0 +1,12 @@
+const http = request('http');
+const host = ' localhost';
+const port = 8080;
+const server = http.createServer((req , res)=>
+{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello cloud computing class!');
+});
+server.listen(port, hostname, ()=>{
+    console.log(`Sever is running at ${port}`);
+})
